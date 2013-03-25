9a189971ebdc736f0a03b6df027cb852df9012c3
Juan I. Benavides <linnk87@gmail.com>
2013-03-25 13:20:10 -0600

    Cropped functionality to resize method.

--- SimpleImage.class.php
+++ SimpleImage.class.php
@@ -5,7 +5,7 @@
  * This is class for when the Image it's not found.
  */
 
-	class ImageNotFoundException extends Exception { }
+class ImageNotFoundException extends Exception { }
 
 /**
  * SimpleImage Class.
@@ -15,169 +15,227 @@
  * Recent modification's author: Linnk.
  */
 
-	class SimpleImage 
+class SimpleImage 
+{
+	private $image;
+	private $image_type;
+
+	public static function load($filename)
 	{
-		private $image;
-		private $image_type;
+		try
+		{
+			return new SimpleImage($filename);
+		}
+		catch(ImageNotFoundException $e)
+		{
+			return null;
+		}
+	}
+	public function __construct($filename)
+	{
+		if(!file_exists($filename))
+			throw new ImageNotFoundException();
 
-		public static function load($filename)
-		{
-			try
-			{
-				return new SimpleImage($filename);
-			}
-			catch(ImageNotFoundException $e)
-			{
-				return null;
-			}
-		}
-		public function __construct($filename)
-		{
-			if(!file_exists($filename))
-				throw new ImageNotFoundException();
+		$image_info = getimagesize($filename);
+		$this->image_type = $image_info[2];
 
-			$image_info = getimagesize($filename);
-			$this->image_type = $image_info[2];
+		if($this->image_type == IMAGETYPE_JPEG)
+		{
+			$this->image = imagecreatefromjpeg($filename);
+		}
+		elseif($this->image_type == IMAGETYPE_GIF)
+		{
+			$this->image = imagecreatefromgif($filename);
+		}
+		elseif($this->image_type == IMAGETYPE_PNG)
+		{
+			$this->image = imagecreatefrompng($filename);
+		}
+		else
+			throw new ImageNotFoundException();
+	}
+	public function __destruct()
+	{
+		imagedestroy($this->image);
+	}
+	public function save($filename, $image_type = null, $compression = 100, $permissions = null)
+	{
+		if(is_null($image_type))
+			$image_type = $this->image_type;
 
-			if($this->image_type == IMAGETYPE_JPEG)
-			{
-				$this->image = imagecreatefromjpeg($filename);
-			}
-			elseif($this->image_type == IMAGETYPE_GIF)
-			{
-				$this->image = imagecreatefromgif($filename);
-			}
-			elseif($this->image_type == IMAGETYPE_PNG)
-			{
-				$this->image = imagecreatefrompng($filename);
-			}
-			else
-				throw new ImageNotFoundException();
-		}
-		public function __destruct()
+		if($image_type == IMAGETYPE_JPEG)
 		{
-			imagedestroy($this->image);
+			imagejpeg($this->image, $filename, $compression);
 		}
-		public function save($filename, $image_type = null, $compression = 100, $permissions = null)
+		elseif($image_type == IMAGETYPE_GIF)
 		{
-			if(is_null($image_type))
-				$image_type = $this->image_type;
+			imagegif($this->image, $filename);         
+		}
+		elseif($image_type == IMAGETYPE_PNG)
+		{
+			imagepng($this->image, $filename);
+		}
 
-			if($image_type == IMAGETYPE_JPEG)
-			{
-				imagejpeg($this->image, $filename, $compression);
-			}
-			elseif($image_type == IMAGETYPE_GIF)
-			{
-				imagegif($this->image, $filename);         
-			}
-			elseif($image_type == IMAGETYPE_PNG)
-			{
-				imagepng($this->image, $filename);
-			}
+		if($permissions != null)
+			chmod($filename, $permissions);
+	}
+	public function output($send_headers = true, $image_type = null)
+	{
+		if(is_null($image_type))
+			$image_type = $this->image_type;
 
-			if($permissions != null)
-				chmod($filename, $permissions);
-		}
-		public function output($image_type = IMAGETYPE_JPEG)
-		{
-			if(is_null($image_type))
-				$image_type = $this->image_type;
+		if($send_headers === true)
+			header('Content-Type: '.$image_type);
 
-			if($image_type == IMAGETYPE_JPEG)
-			{
-				imagejpeg($this->image);
-			}
-			elseif($image_type == IMAGETYPE_GIF)
-			{
-				imagegif($this->image);         
-			}
-			elseif($image_type == IMAGETYPE_PNG)
-			{
-				imagepng($this->image);
-			}   
-		}
-		public function getWidth()
+		if($image_type == IMAGETYPE_JPEG)
 		{
-			return imagesx($this->image);
+			imagejpeg($this->image);
 		}
-		public function getHeight()
+		elseif($image_type == IMAGETYPE_GIF)
 		{
-			return imagesy($this->image);
+			imagegif($this->image);         
 		}
-		public function getImageType()
+		elseif($image_type == IMAGETYPE_PNG)
 		{
-			return $this->image_type;
-		}
-		public function resizeToWidth($width)
-		{
-			$ratio = $width / $this->getWidth();
-			$height = $this->getheight() * $ratio;
+			imagepng($this->image);
+		}   
+	}
+	public function getWidth()
+	{
+		return imagesx($this->image);
+	}
+	public function getHeight()
+	{
+		return imagesy($this->image);
+	}
+	public function getImageType()
+	{
+		return $this->image_type;
+	}
+	public function resizeToWidth($width)
+	{
+		$ratio = $width / $this->getWidth();
+		$height = $this->getHeight() * $ratio;
 
-			$this->resize($width,$height);
-		}
-		public function resizeToHeight($height)
-		{
-			$ratio = $height / $this->getHeight();
-			$width = $this->getWidth() * $ratio;
+		$this->__resize($width,$height);
+	}
+	public function resizeToHeight($height)
+	{
+		$ratio = $height / $this->getHeight();
+		$width = $this->getWidth() * $ratio;
 
-			$this->resize($width,$height);
-		}
-		public function scale($scale)
-		{
-			$width = $this->getWidth() * $scale/100;
-			$height = $this->getheight() * $scale/100; 
+		$this->__resize($width,$height);
+	}
+	public function scale($scale)
+	{
+		$width = $this->getWidth() * $scale/100;
+		$height = $this->getHeight() * $scale/100; 
 
-			$this->resize($width,$height);
-		}
-		public function resize($width, $height)
+		$this->__resize($width,$height);
+	}
+	private function __resize($width, $height)
+	{
+		$new_image = imagecreatetruecolor($width, $height);
+
+		imagealphablending($new_image, true);
+		imagesavealpha($new_image, true);
+		imagefill($new_image, 0, 0, imagecolorallocatealpha($new_image, 244, 244, 244, 127));
+		imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
+
+		$this->image = $new_image;
+	}
+	public function resize($width, $height, $cropped = true)
+	{
+		if($cropped === true)
 		{
 			$new_image = imagecreatetruecolor($width, $height);
 
-			imagealphablending($new_image, true);
-			imagesavealpha($new_image, true);
-			imagefill($new_image, 0, 0, imagecolorallocatealpha($new_image, 244, 244, 244, 127));
-			imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
+			$ratio = $width / $this->getWidth();
+			$new_height = $this->getHeight() * $ratio;
+
+			if($new_height >= $height)
+			{
+				$this->resizeToWidth($width);
+				$x = 0;
+ 				$y = ($height/2 - ($this->getHeight()-$height)/2);
+			}
+			else
+			{
+				$this->resizeToHeight($height);
+ 				$x = ($width/2 - ($this->getWidth()-$width)/2);
+				$y = 0;
+			}
+
+			imagecopy($new_image, $this->image, 0, 0, $x, $y, $this->getWidth(), $this->getHeight());
 
 			$this->image = $new_image;
 		}
-		public function flip($mode = 'both')
+		else
 		{
-			$src_width = $width = $this->getWidth();
-			$src_height = $height = $this->getHeight();
-
-		    $src_y = $src_x = 0;
-
-		    switch ($mode)
-		    {
-		        case 'vertical':
-		            $src_y = $height - 1;
-		            $src_height = $height * (-1);
-					break;
-
-		        case 'horizontal':
-		            $src_x = $width - 1;
-		            $src_width = $width * (-1);
-					break;
-
-		        case 'both':
-		            $src_x = $width - 1;
-		            $src_y = $height - 1;
-		            $src_width = $width * (-1);
-		            $src_height = $height * (-1);
-					break;
-
-		        default:
-		            return false;
-		    }
-
-		    $new_image = imagecreatetruecolor($width, $height);
-
-		    if($success = imagecopyresampled($new_image, $this->image, 0, 0, $src_x, $src_y , $width, $height, $src_width, $src_height))
-				$this->image = $new_image;
-
-			return $success;
+			$this->__resize($width,$height);
 		}
 	}
+	public function convertToBlackAndWhite()
+	{
+		$width = $this->getWidth();
+		$height = $this->getHeight();
 
-?>
+		$bwimage = imagecreate($width, $height); 
+
+		for ($c=0;$c<256;$c++) 
+			$palette[$c] = imagecolorallocate($bwimage,$c,$c,$c);
+
+		for ($y=0;$y<$height;$y++)
+		{
+			for ($x=0;$x<$width;$x++)
+			{
+				$rgb = imagecolorat($this->image, $x, $y);
+				$r = ($rgb >> 16) & 0xFF;
+				$g = ($rgb >> 8) & 0xFF;
+				$b = $rgb & 0xFF;
+
+				$gs = ($r*0.299)+($g*0.587)+($b*0.114);
+				imagesetpixel($bwimage,$x,$y,$palette[$gs]);
+			}
+		}
+
+		$this->image = $bwimage;
+	}
+	public function flip($mode = 'both')
+	{
+		$src_width = $width = $this->getWidth();
+		$src_height = $height = $this->getHeight();
+
+	    $src_y = $src_x = 0;
+
+	    switch ($mode)
+	    {
+	        case 'vertical':
+	            $src_y = $height - 1;
+	            $src_height = $height * (-1);
+				break;
+
+	        case 'horizontal':
+	            $src_x = $width - 1;
+	            $src_width = $width * (-1);
+				break;
+
+	        case 'both':
+	            $src_x = $width - 1;
+	            $src_y = $height - 1;
+	            $src_width = $width * (-1);
+	            $src_height = $height * (-1);
+				break;
+
+	        default:
+	            return false;
+	    }
+
+	    $new_image = imagecreatetruecolor($width, $height);
+
+	    if($success = imagecopyresampled($new_image, $this->image, 0, 0, $src_x, $src_y , $width, $height, $src_width, $src_height))
+			$this->image = $new_image;
+
+		return $success;
+	}
+}
