92e1487c4cc2fdf4ff1287615e27f71021e01c96
Juan I. Benavides <linnk87@gmail.com>
2013-03-25 13:20:59 -0600

    Example of the new functionality added.

--- sample.php
+++ sample.php
@@ -5,22 +5,28 @@
  *
  * SimpleImage::load it's a factory method which returns the instance or
  * null, when the path to the picture is not found.
+ *
+ * Picture from: http://browse.deviantart.com/?qh=&section=&q=cat#/djdwb2
  */
 
 	require('SimpleImage.class.php');
 
-	$image = SimpleImage::load('./picture.jpg'); // Picture from: http://browse.deviantart.com/?qh=&section=&q=cat#/djdwb2
+	$image = SimpleImage::load('./picture.jpg'); 
 	
 	if($image)
 	{
 		$image->flip();
 		$image->save('picture2.jpg');
 
-		$image->resizeToWidth(500);
+		$image->resizeToWidth(400);
 		$image->save('picture3.jpg');
 
-		$image->scale(20);
+		$image->flip();
+		$image->resize(100, 100);
 		$image->save('picture4.jpg');
+		
+		$image->convertToBlackAndWhite();
+		$image->save('picture5.jpg');
 		
 		// The next method just stream to the browser the actual state of the image.
 		// $image->output();
